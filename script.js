147bf365e4549071fcaf3a73274acfe02db317a4
Konstantin <konstantin1020@gmail.com>
2021-01-21 16:29:21 -0800

    pushing all the work up to this point

---
+++ script.js
@@ -0,0 +1,64 @@
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
+var questionElement = document.querySelector (".quiz");
+var resultsElement = document.querySelector (".results");
+var qDisplay = document.querySelector ("#Qs")
+var a1Display = document.querySelector ("#A1")
+var a2Display = document.querySelector ("#A2")
+var a3Display = document.querySelector ("#A3")
+var a4Display = document.querySelector ("#A4")
+
+
+var questionsIndex = 0
+
+
+var questions = [ 
+    {
+        Q: "Which of these IS NOT a variable value?",
+        A: ["string", "number", "prompt", "boolean"],
+        C: "prompt"
+    },
+    {
+        Q: "When a variable lists elements associated with one another, that list is called:",
+        A: ["variety", "array", "inventory", "catalog"],
+        C: "array"
+    }
+]
+
+function displayQuestions (){
+    qDisplay.textContent = questions[questionsIndex].Q
+    a1Display.textContent = questions[questionsIndex].A[0]
+    a2Display.textContent = questions[questionsIndex].A[1]
+    a3Display.textContent = questions[questionsIndex].A[2]
+    a4Display.textContent = questions[questionsIndex].A[3]
+
+}
+
+function startTimer() {
+    timerCount=60
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >5999) {
+      }
+     
+      if (timerCount === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+  }
+
+
+  startButton.addEventListener("click",function(){
+    displayQuestions();
+    startTimer();
+  })
+
+  questionElement.addEventListener("click",function (event){
+
+    if(event.target.matches("button")){
+        console.log(event.target)
+        console.log (event.target.textContent)
+    }
+    
+  })  
